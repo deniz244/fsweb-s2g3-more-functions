eb41b93e8bc98daf06d15f859e04ce2680bfe878
Deniz <77787415+deniz244@users.noreply.github.com>
2023-05-31 13:40:20 +0300

    görev1-3 çözüldü görev2'de eksik var

--- index.js
+++ index.js
@@ -16,10 +16,14 @@
   örnek output: ""
 */
 
-function dosyaAdiniBul(/* kodlar buraya */) {
-  // kodlar buraya
+function dosyaAdiniBul(path) {
+  
+  const yeni = path.split("/");
+  return yeni[yeni.length - 1]
+  //return yeni.slice(-1); ----- [""] veya ["Beethoven_5.mp3"] şeklinde dönüyor
+  
 }
-
+dosyaAdiniBul("C:/Users/johnsmith/Music/Beethoven_5.mp3");
 /*
   GÖREV 2
   - Input:  Bir sayı arrayini parametre olarak alacak bir fonksiyon oluştur.
@@ -38,10 +42,22 @@ function dosyaAdiniBul(/* kodlar buraya */) {
   örnek output: 104
 */
 
-function ortalamaBul(/* kodlar buraya */) {
-  // kodlar buraya
-}
+function ortalamaBul(sDizi) {
 
+  //null kontrolü yok
+
+  let total = 0;
+  let count = 0;
+
+  sDizi.forEach(function(item) {
+      total += item;
+      count++;
+  });
+
+  return total / count;
+  
+}
+ortalamaBul([109, 216, 288, 143, 71, 185, -278, 194, 5])
 /*
   GÖREV 3
   - Input:  Bir sayı arrayini ve ortalama bulmaya yarayacak bir fonksiyonu parametre olarak alacak bir fonksiyon oluştur.
@@ -62,9 +78,15 @@ function ortalamaBul(/* kodlar buraya */) {
   örnek output: [109, 216, 288, 143, 185, 194]
 */
 
-function ortalamadanBuyukleriBul(/* kodlar buraya */) {
-  // kodlar buraya
+function ortalamadanBuyukleriBul(dizi,ortBul) {
+  
+  const ortalamadanBuyukEsit = dizi.filter((eleman) => eleman >= ortBul(dizi))
+  //console.log("ortalama büyük eşit",ortalamadanBuyukEsit)
+
+  return ortalamadanBuyukEsit
 }
+
+ortalamadanBuyukleriBul([50, -26, 153, 7],ortalamaBul)
 
 /* !!!! Burdan aşağısını değiştirmeyin !!!! */
 function as() {
